87f09df00b7c588b4f0ddcf26359ab7e418d7fa3
Harlan-jpg <harlantasci437@gmail.com>
2026-02-25 09:57:48 -0700

    chore: created ios proj

---
+++ iOS/TodoList-FullStack iOS/TodoList_FullStack_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_FullStack_iOSApp.swift
+//  TodoList-FullStack iOS
+//
+//  Created by Harlan Tasci on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_FullStack_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/TodoList-FullStack iOSTests/TodoList_FullStack_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_FullStack_iOSTests.swift
+//  TodoList-FullStack iOSTests
+//
+//  Created by Harlan Tasci on 2/25/26.
+//
+
+import Testing
+@testable import TodoList_FullStack_iOS
+
+struct TodoList_FullStack_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
